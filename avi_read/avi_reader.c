1a79094852ce5c4a592a92091cb35eddb0e8a996
0xAA55 <weixiaobao213@qq.com>
2026-01-16 18:49:06 +0800

    使仅在定位到帧后才调用回调函数，以免往回 seek 的时候触发零帧回调。

--- avi_read/avi_reader.c
+++ avi_read/avi_reader.c
@@ -859,7 +859,7 @@ AVI_FUNC int avi_video_seek_to_frame_index(avi_stream_reader *s, fsize_t frame_i
 	if (call_receive_functions)
 	{
 		if (s->cur_packet_offset == 0)
-			return avi_stream_reader_move_to_next_packet(s, 1);
+			return avi_stream_reader_move_to_next_packet(s, s->cur_stream_packet_index == frame_index);
 		else
 			return avi_stream_reader_call_callback_functions(s);
 	}
