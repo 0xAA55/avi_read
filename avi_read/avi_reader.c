82f4be5d05705b19abce9568ad230e0ebbe9e552
0xAA55 <weixiaobao213@qq.com>
2026-01-26 22:52:15 +0800

    通过精确判断返回值是不是 `-1` 以实现 `seek()`、`tell()` 超过 2GB 的文件，达到 3.999999GB 范围的支持。

--- avi_read/avi_reader.c
+++ avi_read/avi_reader.c
@@ -110,7 +110,7 @@ AVI_FUNC static int must_match(avi_reader *r, const char *fourcc)
 AVI_FUNC static int must_read(avi_reader *r, void *buffer, size_t len)
 {
 	fssize_t rl = r->f_read(buffer, len, r->userdata);
-	if (rl < 0)
+	if (rl == -1)
 	{
 		FATAL_PRINTF(r, "Read %u bytes failed." NL, (unsigned int)len);
 		return 0;
@@ -129,7 +129,7 @@ AVI_FUNC static int must_read(avi_reader *r, void *buffer, size_t len)
 AVI_FUNC static int must_tell(avi_reader *r, fsize_t *cur_pos)
 {
 	fssize_t told = r->f_tell(r->userdata);
-	if (told < 0)
+	if (told == -1)
 	{
 		FATAL_PRINTF(r, "`f_tell()` failed." NL, 0);
 		return 0;
@@ -144,7 +144,7 @@ AVI_FUNC static int must_tell(avi_reader *r, fsize_t *cur_pos)
 AVI_FUNC static int must_seek(avi_reader *r, fsize_t target)
 {
 	fssize_t told = r->f_seek(target, r->userdata);
-	if (told < 0)
+	if (told == -1)
 	{
 		FATAL_PRINTF(r, "`f_seek(%x)` failed." NL, target);
 		return 0;
@@ -170,7 +170,7 @@ AVI_FUNC static int must_match_s(avi_stream_reader *r, const char *fourcc)
 AVI_FUNC static int must_read_s(avi_stream_reader *r, void *buffer, size_t len)
 {
 	fssize_t rl = r->f_read(buffer, len, r->userdata);
-	if (rl < 0)
+	if (rl == -1)
 	{
 		FATAL_PRINTF(r->r, "Read %u bytes failed." NL, (unsigned int)len);
 		return 0;
@@ -189,7 +189,7 @@ AVI_FUNC static int must_read_s(avi_stream_reader *r, void *buffer, size_t len)
 AVI_FUNC static int must_tell_s(avi_stream_reader *r, fsize_t *cur_pos)
 {
 	fssize_t told = r->f_tell(r->userdata);
-	if (told < 0)
+	if (told == -1)
 	{
 		FATAL_PRINTF(r->r, "`f_tell()` failed." NL, 0);
 		return 0;
@@ -204,7 +204,7 @@ AVI_FUNC static int must_tell_s(avi_stream_reader *r, fsize_t *cur_pos)
 AVI_FUNC static int must_seek_s(avi_stream_reader *r, fsize_t target)
 {
 	fssize_t told = r->f_seek(target, r->userdata);
-	if (told < 0)
+	if (told == -1)
 	{
 		FATAL_PRINTF(r->r, "`f_seek(%x)` failed." NL, target);
 		return 0;
